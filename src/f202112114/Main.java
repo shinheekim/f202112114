7e5162a230af873268ee19666cccec250cd8cce8
CINI <shinhee1241@gmail.com>
2023-10-18 10:46:14 +0900

    two

--- src/f202112114/Main.java
+++ src/f202112114/Main.java
@@ -1,6 +1,8 @@
 package f202112114;
+
 public class Main {
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 202112114");
 	}
 }
---
+++ src/f202112114/Two.java
@@ -0,0 +1,7 @@
+package f202112114;
+
+public class Two {
+    public static void print() {
+        System.out.println("two");
+    }
+}
