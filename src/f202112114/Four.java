982060c695f207f6e52e386ecc4ead9f285e14dd
CINI <shinhee1241@gmail.com>
2023-10-18 10:51:33 +0900

    four

---
+++ src/f202112114/Four.java
@@ -0,0 +1,7 @@
+package f202112114;
+
+public class Four {
+    public static void print() {
+        System.out.println("four");
+    }
+}
--- src/f202112114/Main.java
+++ src/f202112114/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		Two.print();
 		System.out.println("one 202112114");
+		Four.print();
 	}
 }
